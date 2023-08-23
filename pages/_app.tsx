9c8d489afc568cab7b0960d37e98dcd0b66ff6c7
bd rex <damilolabamidele53@gmail.com>
2023-08-23 21:20:16 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,42 @@
-
-
-
 import { useEffect } from "react";
 import "../styles/globals.css";
 
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
-import { ThirdwebProvider, ThirdwebSDKProvider } from "@thirdweb-dev/react";
-import { Optimism } from "@thirdweb-dev/chains";
-import { useNetworkMismatch } from "@thirdweb-dev/react";
-import "../styles/globals.css";
 
-const activeChain = "Optimism";
+import { DefaultSeo } from "next-seo";
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { ThirdwebProvider, ThirdwebSDKProvider } from "@thirdweb-dev/react";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-
-  useEffect(() => {
-    const App = () => {
-      const isMismatched = useNetworkMismatch();
-    };
-  });
-
   return (
-    <ThirdwebSDKProvider activeChain={activeChain}>
+    <ThirdwebSDKProvider
+      activeChain={{
+        // === Required information for connecting to the network === \\
+        chainId: 3797, // Chain ID of the network
+        // Array of RPC URLs to use
+        rpc: ["https://alveychain.rpc.thirdweb.com"],
+
+        // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+        // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+        nativeCurrency: {
+          decimals: 18,
+          name: " AlveyChain Mainnet",
+          symbol: "ALV",
+        },
+        shortName: "ALV", // Display value shown in the wallet UI
+        slug: "ALV Mainnet", // Display value shown in the wallet UI
+        testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+        chain: "AlveyChain", // Name of the network
+        name: "  AlveyChain", // Name of the network
+      }}
+    >
       <ThirdwebProvider
         activeChain={{
           // === Required information for connecting to the network === \\
@@ -46,8 +57,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           chain: "AlveyChain", // Name of the network
           name: "  AlveyChain", // Name of the network
         }}
-        clientId={"316ac75a2ee3b40ab6438468663b00b7"}
+        clientId={"b4bb3bbb64fb1ca7ecb6311dab128c76"}
       >
+       
           <Head>
             <title>Title</title>
             <link rel="icon" href="/favicon/favicon.ico" />
@@ -55,13 +67,35 @@ function MyApp({ Component, pageProps }: AppProps) {
             <meta name="msapplication-TileColor" content="#ffffff" />
             <meta name="theme-color" content="#000000" />
           </Head>
-         
+          <DefaultSeo
+            openGraph={{
+              type: "website",
+              locale: "en_US",
+             
+              site_name: "Title",
+              title: "Title",
+              description: "description",
+             
+            }}
+            twitter={{
+              handle: "@",
+              site: "@",
+              cardType: "summary_large_image",
+            }}
+            additionalLinkTags={[
+              {
+                rel: "icon",
+                href: "/favicon/favicon.ico",
+              },
+            ]}
+          />
+      
             <Component {...pageProps} />
+   
+      
       </ThirdwebProvider>
     </ThirdwebSDKProvider>
   );
 }
 
 export default MyApp;
-
-        
